da9c0aaa5d49716dd6180150670ad658406662bf
sweetsky123 <fx1024hi@gmail.com>
2025-07-28 20:29:29 +0800

    修复上传脚本

--- 上传.bat
+++ 上传.bat
@@ -3,8 +3,8 @@ set /p commitMessage="Enter commit message: "
 
 git add .
 git commit -m "%commitMessage%"
-git push
 git push -u origin main
+git push -u cnb HEAD:main
 
 if %errorlevel% neq 0 (
     echo "Git operation failed. Please check for errors."
