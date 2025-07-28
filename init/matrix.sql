1a31e7a4be8d7fc83c8917c32666d3d9e3e78481
sweetsky123 <fx1024hi@gmail.com>
2025-07-28 20:05:21 +0800

    首次提交

---
+++ init/matrix.sql
@@ -0,0 +1,14 @@
+-- 创建matrix用户并设置密码
+CREATE USER matrix WITH PASSWORD 'zR7Kd5AKb3BE7Y5k';
+
+-- 创建使用C排序规则的matrix数据库
+CREATE DATABASE matrix
+    WITH 
+    OWNER = matrix
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TEMPLATE = template0;  -- 使用纯净模板，避免继承默认设置
+
+-- 授予所有权限
+GRANT ALL PRIVILEGES ON DATABASE matrix TO matrix;
